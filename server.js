addc0eeafd3cdb26e138fc9d8ec1f5e8ff3da42e
David Claphan <97066227+davidclaphan@users.noreply.github.com>
2023-12-01 14:31:45 -0800

    create, read, delete slips

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
 
 const BOAT = "Boats";
+const SLIP = "Slips";
+const USER = "Users";
 
 const router = express.Router();
 
@@ -51,10 +53,10 @@ const checkJwt = jwt({
     credentialsRequired: false
   });
 
-/* ------------- Begin Lodging Model Functions ------------- */
-function post_boat(name, type, length, public, owner){
+/* ------------- Begin Model Functions ------------- */
+function post_boat(name, type, length, public=true, slip=null, owner){
     var key = datastore.key(BOAT);
-	const new_boat = {"name": name, "type": type, "length": length, "public": public, "owner": owner};
+	const new_boat = {"name": name, "type": type, "length": length, "public": public, "slip": slip, "owner": owner};
 	return datastore.save({"key":key, "data":new_boat}).then(() => {return key});
 }
 
@@ -62,20 +64,6 @@ function get_owner_boats(owner){
 	const q = datastore.createQuery(BOAT);
 	return datastore.runQuery(q).then( (entities) => {
 			return entities[0].map(fromDatastore).filter( item => item.owner === owner );
-		});
-}
-
-function get_public_boats(){
-	const q = datastore.createQuery(BOAT);
-	return datastore.runQuery(q).then( (entities) => {
-			return entities[0].map(fromDatastore).filter( item => item.public === true );
-		});
-}
-
-function get_owner_public_boats(owner){
-	const q = datastore.createQuery(BOAT);
-	return datastore.runQuery(q).then( (entities) => {
-			return entities[0].map(fromDatastore).filter( item => item.owner === owner && item.public === true );
 		});
 }
 
@@ -94,6 +82,43 @@ function delete_boat(id) {
     const key = datastore.key([BOAT, parseInt(id, 10)]);
     return datastore.delete(key);
 }
+
+/**************************************SLIPS FUNCTIONS ***********************************/
+function post_slip(number, current_boat=null, length, premium) {
+    var key = datastore.key(SLIP);
+    const new_slip = { "number": number, "current_boat": current_boat, "length": length, "premium": premium };
+    return datastore.save({ "key": key, "data": new_slip }).then(() => { return key });
+}
+
+function get_slips() {
+    const q = datastore.createQuery(SLIP);
+    return datastore.runQuery(q).then((entities) => {
+        return entities[0].map(fromDatastore);
+    });
+}
+
+function get_slip(id) {
+    const key = datastore.key([SLIP, parseInt(id, 10)]);
+    return datastore.get(key).then((entity) => {
+        if (entity[0] === undefined || entity[0] === null) {
+            return entity;
+        } else {
+            return entity.map(fromDatastore);
+        }
+    });
+}
+
+function put_slip(id, number, current_boat) {
+    const key = datastore.key([SLIP, parseInt(id, 10)]);
+    const slip = { "number": number, "current_boat": current_boat };
+    return datastore.save({ "key": key, "data": slip }).then(() => { return key });
+}
+
+function delete_slip(id) {
+    const key = datastore.key([SLIP, parseInt(id, 10)]);
+    return datastore.delete(key);
+}
+
 /* ------------- End Model Functions ------------- */
 
 /* ------------- Begin Auth0 Functions ------------- */
@@ -122,10 +147,7 @@ app.get('/getUserData', (req, res) => {
 /* ------------- Begin Controller Functions BOAT API ------------- */
 router.get('/boats', checkJwt, function(req, res){
     if (req.user === undefined) {
-        const boats = get_public_boats()
-        .then ( (boats) => {
-            res.status(200).json(boats);
-        });
+        // RETURN ERROR
     } else {
         const boats = get_owner_boats(req.user.sub)
 	    .then( (boats) => {
@@ -134,35 +156,47 @@ router.get('/boats', checkJwt, function(req, res){
     };
 });
 
-router.get('/owners/:owner_id/boats', function(req, res){
-    get_owner_public_boats(req.params.owner_id)
-	.then( (boats) => {
-        res.status(200).json(boats)
-    });
-});
-
 router.post('/boats', checkJwt, function(req, res){
     if(req.get('content-type') !== 'application/json'){
         res.status(415).send('Server only accepts application/json data.')
     }
     if (req.user === undefined) {
         res.status(401).json({'Error': 'missing or invalid JWT'})
-    }
-    else {
-        post_boat(req.body.name, req.body.type, req.body.length, req.body.public, req.user.sub)
+    } else if (req.body.name === undefined || req.body.type == undefined || req.body.length === undefined) {
+        res.status(400).json({ 'Error': 'The request object is missing at least one of the required attributes' })
+    } else {
+        post_boat(req.body.name, req.body.type, req.body.length, req.user.sub)
         .then( key => {
             const boat_res = {
                 id: parseInt(key.id),
                 name: req.body.name,
                 type: req.body.type,
                 length: req.body.length,
-                public: req.body.public,
-                owner: req.user.name
+                owner: req.user.sub,
+                self: req.protocol + "://" + req.get("host") + "/boats/" + parseInt(key.id)
             }
             res.location(req.protocol + "://" + req.get('host') + req.baseUrl + '/' + key.id);
             res.status(201).json(boat_res);
         } );
     }
+});
+
+// PATCH all Boats NOT ALLOWED
+router.patch('/boats', function (req, res) {
+    res.set('Accept', 'GET, POST');
+    res.status(405).end();
+});
+
+// PUT all Boats NOT ALLOWED
+router.put('/boats', function (req, res) {
+    res.set('Accept', 'GET, POST');
+    res.status(405).end();
+});
+
+// DELETE all Boats NOT ALLOWED
+router.delete('/boats', function (req, res) {
+    res.set('Accept', 'GET, POST');
+    res.status(405).end();
 });
 
 router.delete('/boats/:boat_id', checkJwt, function(req, res) {
@@ -184,6 +218,113 @@ router.delete('/boats/:boat_id', checkJwt, function(req, res) {
     };
 });
 /* ------------- End Controller Functions BOAT API ------------- */
+
+
+
+/* ------------- Begin Controller Functions SLIP API ------------- */
+router.post('/slips', function (req, res) {
+    if (req.body.number === undefined) {
+        res.status(400).json({ 'Error': 'The request object is missing the required number' }).end();
+    } else {
+        post_slip(req.body.number, req.body.current_boat, req.body.length, req.body.premium)
+        .then(key => { 
+            const slip_res = {
+                id: key.id,
+                number: parseInt(req.body.number),
+                current_boat: null,
+                length: req.body.length,
+                premium: req.body.premium,
+                self: req.protocol + "://" + req.get("host") + "/slips/" + parseInt(key.id)
+            };
+            res.status(201).json(slip_res)
+        });
+    }
+});
+
+// Get a Slip
+router.get('/slips/:slip_id', function (req, res) {
+    get_slip(req.params.slip_id)
+    .then(slip => {
+        if (slip[0] === undefined || slip[0] === null) {
+            res.status(404).json({ 'Error': 'No slip with this slip_id exists' });
+        } else {
+            const slip_res = {
+                id: parseInt(req.params.slip_id),
+                number: parseInt(slip[0].number),
+                current_boat: parseInt(slip[0].current_boat),
+                length: parseInt(slip[0].length),
+                premium: slip[0].premium,
+                self: req.protocol + "://" + req.get("host") + "/slips/" + parseInt(req.params.slip_id)
+            };
+            res.status(200).json(slip_res);
+        }
+    });
+});
+
+// List all Slips
+router.get('/slips', function (req, res) {
+    const slips = get_slips()
+        .then((slips) => {
+            res.status(200).json(slips);
+        });
+});
+
+// Delete a Slip
+router.delete('/slips/:slip_id', function (req, res) {
+    get_slip(req.params.slip_id)
+    .then(slip => {
+        if (slip[0] === undefined || slip[0] === null) {
+            res.status(404).json({ 'Error': 'No slip with this slip_id exists' }); 
+        } else {
+            delete_slip(req.params.slip_id).then(res.status(204).end());
+        }
+    });
+});
+
+// Boat Arrives at a Slip
+router.put('/slips/:slip_id/:boat_id', function (req, res) {
+    get_slip(req.params.slip_id)
+    .then(slip => {
+        if (slip[0] === undefined || slip[0] === null) {
+            res.status(404).json({ 'Error': 'The specified boat and/or slip does not exist' }); 
+        } else {
+            get_boat(req.params.boat_id)
+            .then(boat => {
+                if (boat[0] === undefined || boat[0] === null) {
+                    res.status(404).json({ 'Error': 'The specified boat and/or slip does not exist' }); 
+                } else if (slip[0].current_boat !== null) {
+                    res.status(403).json({ 'Error': 'The slip is not empty'});
+                } else {
+                    put_slip(parseInt(req.params.slip_id), parseInt(slip[0].number), parseInt(req.params.boat_id)).then(res.status(204).end());
+                }
+            });
+        }       
+    });
+});
+
+// Boat Departs a Slip
+router.delete('/slips/:slip_id/:boat_id', function (req, res) {
+    get_boat(req.params.boat_id)
+    .then(boat => {
+        if (boat[0] === undefined || boat[0] === null) {
+            res.status(404).json({ 'Error': 'No boat with this boat_id is at the slip with this slip_id' });
+        } else {
+            get_slip(req.params.slip_id)
+            .then(slip => {
+                if (slip[0] === undefined || slip[0] === null) {
+                    res.status(404).json({ 'Error': 'No slip with this slip_id exists' }); 
+                } else if (slip[0].current_boat === null) {
+                    res.status(404).json({ 'Error': 'The slip is already empty'});
+                } else if (parseInt(slip[0].current_boat) !== parseInt(boat[0].id)) {
+                    res.status(404).json({ 'Error': 'No boat with this boat_id is at the slip with this slip_id'});
+                } else {
+                    put_slip(parseInt(req.params.slip_id), parseInt(slip[0].number), null).then(res.status(204).end());
+                }
+            });
+        }
+    });
+});
+/* ------------- End Controller Functions SLIP API ------------- */
 
 app.use('/', router);
 app.enable('trust proxy');
