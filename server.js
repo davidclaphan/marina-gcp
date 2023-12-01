1e3db79c15ad6f344c3d88ec70c791798d7729cf
David Claphan <97066227+davidclaphan@users.noreply.github.com>
2023-12-01 15:40:53 -0800

    express install

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ const USER = "Users";
 
 const router = express.Router();
 
-const CLIENT_ID = 'GQ9Evdrv7pHmJ9YRIHUj79sdDAxRz8OH';
-const CLIENT_SECRET = 'BiOzbCyTBMgWCa-nAJxdajZserkdICcKQzjyGa6Y8VIX_CrCG4Mk3eHKTbTfnYRt';
+const CLIENT_ID = 'AuWovbf3d5L2dIxclVdrVHBX3u6v5dqW';
+const CLIENT_SECRET = '-J5HUOB_tBkjhmpeb5Dkm1Y8RBHqgO1Pw8spfkmkGIZ-MpTQS3-k3ZOyIwM_fQDm';
 const DOMAIN = 'dev-j7os5d7uf88tjkip.us.auth0.com';
 
 const config = {
@@ -54,9 +54,9 @@ const checkJwt = jwt({
   });
 
 /* ------------- Begin Model Functions ------------- */
-function post_boat(name, type, length, public=true, slip=null, owner){
+function post_boat(name, type, length, owner, slip=null, public=true){
     var key = datastore.key(BOAT);
-	const new_boat = {"name": name, "type": type, "length": length, "public": public, "slip": slip, "owner": owner};
+	const new_boat = {"name": name, "type": type, "length": length, "owner": owner, "slip": slip, "public": public};
 	return datastore.save({"key":key, "data":new_boat}).then(() => {return key});
 }
 
@@ -76,6 +76,23 @@ function get_boat(id) {
             return entity.map(fromDatastore);
         }
     });
+}
+
+function get_boat_names() {
+    const q = datastore.createQuery(BOAT);
+    return datastore.runQuery(q).then((entities) => {
+        let boat_names = []
+        for (let x in entities[0]) {
+            boat_names.push(entities[0][x].name.toLowerCase())
+        }
+        return boat_names;
+    });
+}
+
+function put_boat(id, name, type, length, owner, slip, public=true) {
+    const key = datastore.key([BOAT, parseInt(id, 10)]);
+    const boat = { "name": name, "type": type, "length": length, "owner": owner, "slip": slip, "public": public,  };
+    return datastore.save({ "key": key, "data": boat }).then(() => { return key });
 }
 
 function delete_boat(id) {
@@ -108,9 +125,9 @@ function get_slip(id) {
     });
 }
 
-function put_slip(id, number, current_boat) {
+function put_slip(id, number, current_boat, length, premium) {
     const key = datastore.key([SLIP, parseInt(id, 10)]);
-    const slip = { "number": number, "current_boat": current_boat };
+    const slip = { "number": number, "current_boat": current_boat, "length": length, "premium": premium };
     return datastore.save({ "key": key, "data": slip }).then(() => { return key });
 }
 
@@ -147,7 +164,7 @@ app.get('/getUserData', (req, res) => {
 /* ------------- Begin Controller Functions BOAT API ------------- */
 router.get('/boats', checkJwt, function(req, res){
     if (req.user === undefined) {
-        // RETURN ERROR
+        res.status(401).json({'Error': 'missing or invalid JWT'})
     } else {
         const boats = get_owner_boats(req.user.sub)
 	    .then( (boats) => {
@@ -165,20 +182,31 @@ router.post('/boats', checkJwt, function(req, res){
     } else if (req.body.name === undefined || req.body.type == undefined || req.body.length === undefined) {
         res.status(400).json({ 'Error': 'The request object is missing at least one of the required attributes' })
     } else {
-        post_boat(req.body.name, req.body.type, req.body.length, req.user.sub)
-        .then( key => {
-            const boat_res = {
-                id: parseInt(key.id),
-                name: req.body.name,
-                type: req.body.type,
-                length: req.body.length,
-                owner: req.user.sub,
-                self: req.protocol + "://" + req.get("host") + "/boats/" + parseInt(key.id)
-            }
-            res.location(req.protocol + "://" + req.get('host') + req.baseUrl + '/' + key.id);
-            res.status(201).json(boat_res);
-        } );
-    }
+        get_boat_names()
+        .then(boat_names => {
+            const accepts = req.accepts('application/json')
+            if (accepts !== 'application/json') {
+                res.status(406).send('MIME Not Acceptable, application/json only')
+            } else if (boat_names.includes(req.body.name.toLowerCase())) {
+                res.status(403).json({ 'Error': 'Boat with this name already exists' }).end();
+            } else {
+                post_boat(req.body.name, req.body.type, req.body.length, req.user.sub)
+                .then( key => {
+                    const boat_res = {
+                        id: parseInt(key.id),
+                        name: req.body.name,
+                        type: req.body.type,
+                        length: req.body.length,
+                        owner: req.user.sub,
+                        slip: null,
+                        self: req.protocol + "://" + req.get("host") + "/boats/" + parseInt(key.id)
+                    }
+                    res.location(req.protocol + "://" + req.get('host') + req.baseUrl + '/' + key.id);
+                    res.status(201).json(boat_res);
+                } );
+            };
+        });
+    };
 });
 
 // PATCH all Boats NOT ALLOWED
@@ -198,6 +226,106 @@ router.delete('/boats', function (req, res) {
     res.set('Accept', 'GET, POST');
     res.status(405).end();
 });
+
+// Edit all attributes for a Boat
+router.put('/boats/:boat_id', checkJwt, function (req, res) {
+    if(req.get('content-type') !== 'application/json'){
+        res.status(415).send('Server only accepts application/json data.')
+    }
+    if (req.user === undefined) {
+        res.status(401).json({'Error': 'missing or invalid JWT'})
+    } else {
+        get_boat(req.params.boat_id)
+        .then(boat => {
+            const accepts = req.accepts('application/json')
+            if (accepts !== 'application/json') {
+                res.status(406).send('MIME Not Acceptable, application/json only')
+            } else if (req.get('Content-Type') !== 'application/json') {
+                res.status(415).json({ 'Error': 'The request object must be JSON format' })
+            } else if (req.body.name === undefined || req.body.type == undefined || req.body.length === undefined) {
+                res.status(400).json({ 'Error': 'The request object is missing at least one of the required attributes, to update individual attributes use PATCH' });
+            } else if (typeof(req.body.name) !== 'string' || typeof(req.body.type) !== 'string' || typeof(req.body.length) !== 'number') {
+                res.status(400).json({ 'Error': 'Data in request object incorrect type. Expected {"name": string, "type": string, "length": number'});
+            } else if (boat[0] === undefined || boat[0] === null) {
+                res.status(404).json({ 'Error': 'No boat with this boat_id exists' });
+            } else {
+                get_boat_names()
+                .then(boat_names => {
+                    if (boat_names.includes(req.body.name.toLowerCase())) {
+                        res.status(403).json({ 'Error': 'Boat with this name already exists' }).end();
+                    } else {
+                        put_boat(req.params.boat_id, req.body.name, req.body.type, req.body.length, request.user.sub, boat[0].slip)
+                        res.set("Location", req.protocol + "://" + req.get("host") + "/boats/" + boat[0].id);
+                        res.status(303).end(); 
+                    }
+                });
+            }
+        });
+    };
+});
+
+// Edit some or all attributes for a Boat
+router.patch('/boats/:boat_id', checkJwt, function (req, res) {
+    if(req.get('content-type') !== 'application/json'){
+        res.status(415).send('Server only accepts application/json data.')
+    }
+    if (req.user === undefined) {
+        res.status(401).json({'Error': 'missing or invalid JWT'})
+    } else {
+        get_boat(req.params.boat_id)
+        .then(boat => {
+            const accepts = req.accepts('application/json')
+            if (accepts !== 'application/json') {
+                res.status(406).send('MIME Not Acceptable, application/json only')
+            } else if (req.get('Content-Type') !== 'application/json') {
+                res.status(415).json({ 'Error': 'The request object must be JSON format' })
+            } else if (boat[0] === undefined || boat[0] === null) {
+                res.status(404).json({ 'Error': 'No boat with this boat_id exists' });
+            } else {
+                get_boat_names()
+                .then(boat_names => {
+                    if (req.body.name !== undefined && typeof(req.body.name) !== 'string') {
+                        res.status(400).json({ 'Error': 'Data in request object incorrect type. Expected name to be string'});
+                    } else if (req.body.type !== undefined && typeof(req.body.type) !== 'string') {
+                        res.status(400).json({ 'Error': 'Data in request object incorrect type. Expected type to be string'});
+                    } else if (req.body.length !== undefined && typeof(req.body.length) !== 'number') {
+                        res.status(400).json({ 'Error': 'Data in request object incorrect type. Expected length to be number'});
+                    } else if (req.body.name !== undefined && boat_names.includes(req.body.name.toLowerCase())) {
+                        res.status(403).json({ 'Error': 'Boat with this name already exists' }).end();
+                    } else {
+                        let name = boat[0].name
+                        let type = boat[0].type
+                        let length = boat[0].length
+            
+                        if (req.body.name !== undefined) {
+                            name = req.body.name
+                        }
+                        if (req.body.type !== undefined) {
+                            type = req.body.type
+                        }
+                        if (req.body.length !== undefined) {
+                            length = req.body.length
+                        }
+            
+                        const boat_res = {
+                            id: req.params.boat_id,
+                            name: name,
+                            type: type,
+                            length: length,
+                            owner: req.user.sub,
+                            slip: boat[0].slip,
+                            self: req.protocol + "://" + req.get("host") + "/boats/" + req.params.boat_id
+                        };
+                        put_boat(req.params.boat_id, name, type, length, owner, boat[0].slip)
+                        res.set("Location", req.protocol + "://" + req.get("host") + "/boats/" + boat[0].id);
+                        res.status(200).json(boat_res); 
+                    }
+                });
+            }}
+        );
+    };
+});
+
 
 router.delete('/boats/:boat_id', checkJwt, function(req, res) {
     if (req.user === undefined) {
@@ -254,7 +382,7 @@ router.get('/slips/:slip_id', function (req, res) {
                 current_boat: parseInt(slip[0].current_boat),
                 length: parseInt(slip[0].length),
                 premium: slip[0].premium,
-                self: req.protocol + "://" + req.get("host") + "/slips/" + parseInt(req.params.slip_id)
+                self: req.protocol + "://" + req.get("host") + "/slips/" + req.params.slip_id
             };
             res.status(200).json(slip_res);
         }
@@ -268,6 +396,85 @@ router.get('/slips', function (req, res) {
             res.status(200).json(slips);
         });
 });
+
+// Edit All attributes for a Slip
+router.put('/slips/:slip_id', function (req, res) {
+    get_slip(req.params.slip_id)
+    .then(slip => {
+        const accepts = req.accepts('application/json')
+        if (accepts !== 'application/json') {
+            res.status(406).send('MIME Not Acceptable, application/json only')
+        } else if (slip[0] === undefined || slip[0] === null) {
+            res.status(404).json({ 'Error': 'A slip with that slip_id does not exist' }); 
+        } else if (req.body.number === undefined || req.body.length == undefined || req.body.premium === undefined) {
+            res.status(400).json({ 'Error': 'The request object is missing at least one of the required attributes' })
+        } else if (typeof(req.body.number) !== 'number' || typeof(req.body.length) !== 'number' || typeof(req.body.premium) !== 'boolean') {
+            res.status(400).json({ 'Error': 'Data in request object incorrect type. Expected {"number": number, "length": number, "length": boolean'});
+        } else {
+            const slip_res = {
+                id: parseInt(req.params.slip_id),
+                number: parseInt(req.body.number),
+                current_boat: slip[0].current_boat,
+                length: parseInt(req.body.length),
+                premium: req.body.premium,
+                self: req.protocol + "://" + req.get("host") + "/slips/" + req.params.slip_id
+            }
+            put_slip(req.params.slip_id, req.body.number, slip[0].current_boat, req.body.length, req.body.premium)
+            res.status(200).json(slip_res)
+        };       
+    });
+});
+
+
+// Edit some or all attributes for a Slip
+router.patch('/slips/:slip_id', function (req, res) {
+    const accepts = req.accepts('application/json')
+    if (accepts !== 'application/json') {
+        res.status(406).send('MIME Not Acceptable, application/json only')
+    } else if (req.get('Content-Type') !== 'application/json') {
+        res.status(415).json({ 'Error': 'The request object must be JSON format' })
+    } else {
+        get_slip(req.params.slip_id)
+        .then(slip => {
+            if (slip[0] === undefined || slip[0] === null) {
+            res.status(404).json({ 'Error': 'A slip with that slip_id does not exist' }); 
+            } else if (req.body.number !== undefined && typeof(req.body.number) !== 'number') {
+                res.status(400).json({ 'Error': 'Data in request object incorrect type. Expected name to be string'});
+            } else if (req.body.length !== undefined && typeof(req.body.length) !== 'number') {
+                res.status(400).json({ 'Error': 'Data in request object incorrect type. Expected length to be number'});
+            } else if (req.body.premium !== undefined && typeof(req.body.premium) !== 'string') {
+                res.status(400).json({ 'Error': 'Data in request object incorrect type. Expected type to be string'});
+            } else {
+                let name = slip[0].number
+                let current_boat = slip[0].current_boat
+                let length = slip[0].length
+                let premium = slip[0].premium
+    
+                if (req.body.number !== undefined) {
+                    name = req.body.number
+                }
+                if (req.body.length !== undefined) {
+                    length = req.body.length
+                }
+                if (req.body.premium !== undefined) {
+                    premium = req.body.premium
+                }
+    
+                const slip_res = {
+                    id: req.params.slip_id,
+                    number: number,
+                    current_boat: current_boat,
+                    length: length,
+                    premium: premium,
+                    self: req.protocol + "://" + req.get("host") + "/slips/" + req.params.slip_id
+                };
+                put_boat(req.params.boat_id, name, type, length, owner, boat[0].slip)
+                res.set("Location", req.protocol + "://" + req.get("host") + "/slips/" + slip[0].id);
+                res.status(200).json(slip_res); 
+            }
+        });
+    }}
+);
 
 // Delete a Slip
 router.delete('/slips/:slip_id', function (req, res) {
